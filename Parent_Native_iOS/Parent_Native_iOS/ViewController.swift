a090e4f9efd7c90aed585f50e95e5254f516591c
nuhin13 <nuhin13@gmail.com>
2023-09-05 19:28:37 +0600

    Initiate Parent iOS App

---
+++ Parent_Native_iOS/Parent_Native_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parent_Native_iOS
+//
+//  Created by Polygon Technology on 5/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
