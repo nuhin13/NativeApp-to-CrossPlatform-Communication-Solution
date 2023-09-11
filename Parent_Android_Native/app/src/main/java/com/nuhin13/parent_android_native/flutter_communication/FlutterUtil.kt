7562fb5e0d39f17827947507f26b1185864c9f91
nuhin13 <nuhin13@gmail.com>
2023-09-11 16:00:06 +0600

    [Edited] FlutterUtil.kt class where Native android can communicate with flutter module.

--- Parent_Android_Native/app/src/main/java/com/nuhin13/parent_android_native/flutter_communication/FlutterUtil.kt
+++ Parent_Android_Native/app/src/main/java/com/nuhin13/parent_android_native/flutter_communication/FlutterUtil.kt
@@ -1,6 +1,8 @@
 package com.nuhin13.parent_android_native.flutter_communication
 
 import android.content.Context
+import android.util.Log
+import com.google.gson.Gson
 import com.nuhin13.parent_android_native.flutter_communication.ChannelConstants.Companion.KEY_ENGINE_ID
 import com.nuhin13.parent_android_native.flutter_communication.ChannelConstants.Companion.KEY_MAIN_CHANNEL
 import io.flutter.embedding.engine.FlutterEngine
@@ -10,28 +12,40 @@ import io.flutter.plugin.common.MethodChannel
 
 object FlutterUtil {
 
+    /**
+     * Start the Flutter engine. This is a singleton method.
+     * Call this method from the Application class or at the entry point of your application.
+     * Start executing Dart code to pre-warm the FlutterEngine.
+     * Cache the FlutterEngine to be used by FlutterActivity.
+     */
     fun startEngine(context: Context) {
         try {
             FlutterEngineCache.getInstance().get(KEY_ENGINE_ID)?.destroy()
             FlutterEngineCache.getInstance().remove(KEY_ENGINE_ID)
             val flutterEngine = FlutterEngine(context)
 
-            // Start executing Dart code to pre-warm the FlutterEngine.
             flutterEngine.dartExecutor.executeDartEntrypoint(DartExecutor.DartEntrypoint.createDefault())
 
-            // Cache the FlutterEngine to be used by FlutterActivity.
             FlutterEngineCache
                 .getInstance()
                 .put(KEY_ENGINE_ID, flutterEngine)
             callMethodChannel(flutterEngine, StartUpMethodChannel())
         } catch (e: Exception) {
+            Log.e("Start Engine Error", e.toString())
         }
     }
 
-    fun callMethodChannel(flutterEngine: FlutterEngine?, handler: MethodChannel.MethodCallHandler): MethodChannel? {
+    /**
+     * Initiate the Method Channel. This is a singleton method.
+     * This the main method channel that will be used to communicate with the Flutter module.
+     */
+
+    fun callMethodChannel(
+        flutterEngine: FlutterEngine?,
+        handler: MethodChannel.MethodCallHandler
+    ): MethodChannel? {
         return try {
             val channel = KEY_MAIN_CHANNEL
-            // Pass message to flutter module
             val methodChannel =
                 flutterEngine?.dartExecutor?.binaryMessenger?.let { MethodChannel(it, channel) }
 
@@ -42,17 +56,61 @@ object FlutterUtil {
         }
     }
 
+    /**
+     * Navigate to the Flutter module
+     */
+
     fun navigateToFlutter(context: Context, toFlutter: String) {
         try {
-            FlutterModuleGenerator.instance?.flutterFeature = toFlutter
+            FlutterModuleGenerator.instance!!.flutterFeature = toFlutter
             context.startActivity(
                 FlutterContainerActivity
                     .withCachedEngine(KEY_ENGINE_ID)
                     .destroyEngineWithActivity(false)
                     .build(context)
             )
-            //(context as Activity).overridePendingTransition(R.anim.right_in, R.anim.left_out)
         } catch (e: Exception) {
+            Log.e("Navigate To Flutter Error", e.toString())
+        }
+    }
+
+
+    /**
+     * Generic method to navigate to the Flutter module with data
+     */
+
+    fun <T> navigateFlutterWithOnlyData(item: T, navigationRoute: String) {
+        try {
+            val jsonString = Gson().toJson(item)
+            val channel = FlutterModuleGenerator.instance!!.methodChannel
+            channel!!.invokeMethod(navigationRoute, jsonString)
+        } catch (e: Exception) {
+            Log.e("Navigate Flutter With Only Data Error", e.toString())
+        }
+
+    }
+
+    /**
+     * Navigate with Specific Screen with data
+     */
+
+    fun navigateFlutterScreenWithData(
+        context: Context, arguments: MutableMap<String, String?>,
+        navigationRoute: String
+    ) {
+        try {
+            val engine = FlutterEngineCache.getInstance().get(KEY_ENGINE_ID)
+            val channel = callMethodChannel(engine, MainMethodChannel(context))
+
+            if (channel != null) {
+                FlutterModuleGenerator.instance?.methodChannel = channel
+
+                print("from parent$arguments")
+
+                channel.invokeMethod(navigationRoute, arguments)
+            }
+        } catch (e: Exception) {
+            Log.e("Navigate Flutter Screen With Data Error", e.toString())
         }
     }
 }
