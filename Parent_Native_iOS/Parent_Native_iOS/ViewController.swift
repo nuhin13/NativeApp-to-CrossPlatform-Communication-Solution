025fc9b6133f9e92f9284be71a45cf334eb793d1
nuhin13 <nuhin13@gmail.com>
2023-09-12 14:21:37 +0600

    [ADD] iOS UI design & reference pass to ViewController

--- Parent_Native_iOS/Parent_Native_iOS/ViewController.swift
+++ Parent_Native_iOS/Parent_Native_iOS/ViewController.swift
@@ -9,6 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var editTextName: UITextField!
+    @IBOutlet weak var editTextPhone: UITextField!
+    @IBOutlet weak var editTextValue1: UITextField!
+    @IBOutlet weak var editTextValue2: UITextField!
+    
+    @IBOutlet weak var btnNavigateFlutter: UIButton!
+    @IBOutlet weak var btnCameraPick: UIButton!
+    @IBOutlet weak var btnCalculateIntoFlutter: UIButton!
+    @IBOutlet weak var btnNavigateFlutterWithImage:UIButton!
+    
+    @IBOutlet weak var imgPreview: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
