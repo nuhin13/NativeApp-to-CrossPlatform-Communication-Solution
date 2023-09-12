4ea8da7345bb044d7cfcf1251bd7128682e00c54
nuhin13 <nuhin13@gmail.com>
2023-09-12 18:57:18 +0600

    [ADD] navigate to flutter module form view controller

---
+++ Parent_Native_iOS/Parent_Native_iOS/FlutterCommunication/MethodChannelConstant.swift
@@ -0,0 +1,45 @@
+//
+//  MethodChannelConstant.swift
+//  Parent_Native_iOS
+//
+//  Created by nuhin13 on 12/9/23.
+//
+
+import Foundation
+
+class MethodChannelConstant {
+    
+    static var KEY_ENGINE_ID = "flutter_engine";
+    static var KEY_MAIN_CHANNEL = "main_flutter_channel";
+    static var IMAGE_CONSTANT_CODE = 124;
+
+    //Navigation from Native to Flutter
+    static var KEY_NATIVE_TO_FLUTTER_OBJECT_PASS = "key_native_to_flutter_object_pass";
+    static var KEY_NATIVE_TO_FLUTTER_SPECIFIC_ROUTE = "key_native_to_flutter_specific_route";
+    static var KEY_NATIVE_TO_FLUTTER_SPECIFIC_ROUTE_WITH_DATA = "key_native_to_flutter_specific_route_with_data";
+    static var KEY_NATIVE_TO_FLUTTER_IMAGE_PASS = "key_native_to_flutter_image_pass";
+    static var KEY_NATIVE_TO_FLUTTER_OPEN = "key_native_to_flutter_open";
+
+    // Navigation from Flutter to Native
+    static var KEY_FLUTTER_TO_NATIVE_OBJECT_PASS = "key_flutter_to_native_object_pass";
+    static var KEY_FLUTTER_TO_NATIVE_SPECIFIC_ROUTE = "key_flutter_to_native_specific_route";
+    static var KEY_FLUTTER_TO_NATIVE_EXIT_FLUTTER = "key_flutter_to_native_exit_flutter";
+    
+//    // Navigation from Native to Flutter
+//    static var KEY_EKYC_NATIVE_TO_FLUTTER = "keyEkycNativeToFlutter";
+//    static var KEY_EKYC_API_NATIVE_TO_FLUTTER = "keyEkycApiNativeToFlutter";
+//    static var KEY_EKYC_UPDATE_NATIVE_TO_FLUTTER = "keyEkycUpdateNativeToFlutter";
+//    static var KEY_LIVELINESS_NATIVE_TO_FLUTTER = "keyLivelinessNativeToFlutter";
+//
+//    // Navigation from Flutter to Native
+//    static var KEY_EKYC_API_FLUTTER_TO_NATIVE_SUCCESS = "keyEkycApiFlutterToNativeSuccess";
+//    static var KEY_EKYC_API_FLUTTER_TO_NATIVE_ERROR = "keyEkycApiFlutterToNativeError";
+//
+//    static var KEY_EKYC_UPDATE_FLUTTER_TO_NATIVE = "keyEkycUpdateFlutterToNative";
+//
+//    static var KEY_LIVELINESS_FLUTTER_TO_NATIVE_SUCCESS = "keyLivelinessFlutterToNativeSuccess";
+//    static var KEY_LIVELINESS_FLUTTER_TO_NATIVE_ERROR = "keyLivelinessFlutterToNativeError";
+//
+//    static var FLUTTER_ENGINE = "agent_flutter_engine";
+//    static var FLUTTER_METHOD_CHANNEL = "main_channel";
+}
